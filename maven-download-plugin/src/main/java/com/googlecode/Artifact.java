9ef486a5f5d5b46c7c0f2b7016e63a1482b4fc26
Marc-Andre Houle <mhoule@gmail.com>
2009-05-11 21:02:12 +0000

    First implementation of the download artifact command

--- maven-download-plugin/src/main/java/com/googlecode/Artifact.java
+++ maven-download-plugin/src/main/java/com/googlecode/Artifact.java
@@ -19,7 +19,14 @@
 package com.googlecode;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -39,11 +46,41 @@ public class Artifact extends AbstractMojo {
 	/**
 	 * The artifact Id of the file to download.
 	 * 
-	 * @parameter expression="${download.artifact.artifactId}"
+	 * @parameter expression="${artifactId}"
 	 * @required
 	 */
 	private String artifactId;
-
+	
+	/**
+	 * The group Id of the file to download.
+	 * 
+	 * @parameter expression="${groupId}"
+	 * @required
+	 */
+	private String groupId;
+	
+	/**
+	 * The version of the file to download.
+	 * 
+	 * @parameter expression="${version}"
+	 * @required
+	 */
+	private String version;
+	
+	/**
+	 * The type of artifact to download.
+	 * 
+	 * @parameter expression="${type}" default-value=jar
+	 */
+	private String type;
+	
+	/**
+	 * The classifier of artifact to download.
+	 * 
+	 * @parameter expression="${classifier}"
+	 */
+	private String classifier;
+	
 	/**
 	 * Location of the file.
 	 * 
@@ -51,6 +88,19 @@ public class Artifact extends AbstractMojo {
 	 * @required
 	 */
 	private File outputDirectory;
+	
+	/** @component */
+	private ArtifactFactory artifactFactory;
+
+	/** @component */
+	private ArtifactResolver resolver;
+
+	/**@parameter expression="${localRepository}" */
+	private ArtifactRepository localRepository;
+
+	/** @parameter expression="${project.remoteArtifactRepositories}" */
+	private java.util.List remoteRepositories;
+
 
 	/*
 	 * (non-Javadoc)
@@ -58,8 +108,28 @@ public class Artifact extends AbstractMojo {
 	 * @see org.apache.maven.plugin.Mojo#execute()
 	 */
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		// TODO Auto-generated method stub
-
+		org.apache.maven.artifact.Artifact artifact = artifactFactory.createArtifactWithClassifier(groupId, artifactId, version, type, classifier);
+		try {
+			resolver.resolve( artifact, remoteRepositories, localRepository );
+		} catch (ArtifactResolutionException e) {
+			getLog().debug("Artifact could not be resolved.", e);
+			throw new MojoFailureException("Artifact could not be resolved.");
+		} catch (ArtifactNotFoundException e) {
+			getLog().debug("Artifact could not be found.", e);
+			throw new MojoFailureException("Artifact could not be found.");
+		}
+		File toCopy = artifact.getFile();
+		if(toCopy != null && toCopy.exists() && toCopy.isFile()){
+			try {
+				getLog().info("Copying file " + toCopy.getName() + " to directory " + outputDirectory);
+				FileUtils.copyFileToDirectory(toCopy, outputDirectory);
+			} catch (IOException e) {
+				getLog().debug("Error while copying file", e);
+				throw new MojoFailureException("Error copying the file : " + e.getMessage());
+			}
+		}else{
+			throw new MojoFailureException("Artifact file not present : " + toCopy);
+		}
 	}
 
 }
