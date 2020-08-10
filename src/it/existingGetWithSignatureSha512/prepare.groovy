e288054c558264b4f6956adec796323da2a7d970
Paul Polishchuk <paul.polishchuk@proemion.com>
2020-08-10 22:59:53 +0300

    fix broken link in ITs

--- src/it/existingGetWithSignatureSha512/prepare.groovy
+++ src/it/existingGetWithSignatureSha512/prepare.groovy
@@ -4,7 +4,7 @@ if (!target.exists()) {
   assert target.exists() : "Folder $target.absolutePath must exist"
 }
 
-File f = new File(basedir, "target/lolcatsdotcomcm90ebvhwphtzqvf.jpg")
+File f = new File(basedir, "target/687474703a2f2f696d672e71756c6963652e636f6d2f6c6f676f2d6269672e706e67")
 if (f.exists()) {
   f.delete()
 }
