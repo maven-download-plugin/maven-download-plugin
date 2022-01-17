712f016437cd445ad295c52966dafaaf9c1bf0e2
Markus KARG <markus@headcrashing.eu>
2022-01-17 17:26:59 +0000

    Fixed #204
    Signed-off-by: Markus KARG <markus@headcrashing.eu>

---
+++ src/it/basicGetWithQuery/verify.groovy
@@ -0,0 +1,3 @@
+File f = new File(basedir, "target/687474703a2f2f696d672e71756c6963652e636f6d2f6c6f676f2d6269672e706e67")
+assert f.exists() : "File $f.absolutePath does not exist"
+assert f.length() > 0 : "File is empty"
