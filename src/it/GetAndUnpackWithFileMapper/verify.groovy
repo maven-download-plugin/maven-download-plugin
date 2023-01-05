907fc7369899e2bd60068bb1e69d0d10d3fa648c
Bill Neff <bneff@imds.tv>
2023-01-04 22:10:42 -0500

    Switch to downloading https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.zip for integration tests

--- src/it/GetAndUnpackWithFileMapper/verify.groovy
+++ src/it/GetAndUnpackWithFileMapper/verify.groovy
@@ -1,5 +1,5 @@
-def f = new File(basedir, "target/apache-maven-3.8.6.zip")
+def f = new File(basedir, "target/apache-maven-3.8.7.zip")
 assert !f.exists()
-f = new File(basedir, "target/apache-maven-3.8.6/RENAMED.txt")
+f = new File(basedir, "target/apache-maven-3.8.7/RENAMED.txt")
 assert f.exists()
 assert f.length() > 0
