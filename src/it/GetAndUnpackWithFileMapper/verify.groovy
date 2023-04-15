bc67a7cbe3f00a1ce46c6e6b2526679ee42d1f9c
Andrzej Jarmoniuk <gh@jarmoniuk.nl>
2023-04-15 15:40:18 +0200

    Replacing external resources with resources hosted locally. Replaced more integration tests with unit test wih Wiremocks.

--- src/it/GetAndUnpackWithFileMapper/verify.groovy
+++ src/it/GetAndUnpackWithFileMapper/verify.groovy
@@ -1,5 +1,2 @@
-def f = new File(basedir, "target/apache-maven-3.8.6.zip")
-assert !f.exists()
-f = new File(basedir, "target/apache-maven-3.8.6/RENAMED.txt")
-assert f.exists()
-assert f.length() > 0
+def f = new File(basedir, "target/howdy.txt")
+assert f.text == "Hello, world!"
--- src/it/basicGetWithRedirect/verify.groovy
+++
@@ -1,3 +0,0 @@
-File f = new File(basedir, "target/version.txt")
-assert f.exists() : "File $f.absolutePath does not exist"
-assert f.length() > 0 : "File is empty"
