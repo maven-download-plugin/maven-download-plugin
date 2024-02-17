15603f611b59296d0c7c8b3e467d25aa3b9a75d8
Smaarn <antoine.malliarakis@gmail.com>
2024-02-17 16:07:30 +0100

    MR feedbacks: updated integration tests to use jar resource containing several files

--- src/it/GetAndUnpackWithIncludes/verify.groovy
+++ src/it/GetAndUnpackWithIncludes/verify.groovy
@@ -1,2 +1,11 @@
-def f = new File(basedir, "target/hello.txt")
-assert f.text == "Hello, world!"
+def f = new File(basedir, "target/first-directory/first-file.txt")
+assert f.text == "Oups"
+
+f = new File(basedir, "target/second-directory/second-file.txt")
+assert f.text == "I"
+
+f = new File(basedir, "target/third-file.txt")
+assert f.text == "did"
+
+f = new File(basedir, "target/fourth-file.html")
+assert !f.exists()
