daaa20b1d6963b8c08694642cbd39a7b75046965
Shane St Savage <shane@axds.co>
2024-10-31 00:03:13 -0700

    Force unpack if unpack=true, even when unpackWhenChanged=true (#285)
    Small change to always unpack an unchanged cached archive
    if unpack is true, even if unpackWhenChanged is true. This allows
    users to force unpacking when the archives are cached/unchanged
    but the unpack target directory doesn't already contain the
    unpacked data (example Github Actions/CI builds).

--- src/it/GetAndUnpackWhenChanged/verify.groovy
+++ src/it/GetAndUnpackWhenChanged/verify.groovy
@@ -3,3 +3,4 @@ assert f.text == "Hello, world!"
 def log = new File(basedir, "build.log")
 assert log.exists() : "$log does not exist"
 assert log.text =~ /Skipping unpacking as the file has not changed/ : "$log does not contain unpack skip message"
+assert log.text =~ /Unpacking even though unchanged cache file exists/ : "$log does not contain force unpack message"
