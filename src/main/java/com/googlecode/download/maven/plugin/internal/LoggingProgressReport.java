9b5189e679e78116d431300e88cc96b7e2dcc6b0
Paul Polishchuk <longtimeago@users.noreply.github.com>
2024-11-18 13:33:34 +0200

    #291 Fix checkstyle violations (part 2) (#292)

--- src/main/java/com/googlecode/download/maven/plugin/internal/LoggingProgressReport.java
+++ src/main/java/com/googlecode/download/maven/plugin/internal/LoggingProgressReport.java
@@ -16,60 +16,106 @@
 package com.googlecode.download.maven.plugin.internal;
 
 import java.net.URI;
-
+import javax.annotation.concurrent.NotThreadSafe;
 import org.apache.maven.plugin.logging.Log;
 
 /**
  * {@link ProgressReport} implementation that logs operation progress at INFO priority.
  */
+@NotThreadSafe
 public final class LoggingProgressReport implements ProgressReport {
 
+    /**
+     * Represents the number of bytes in a kilobyte, used for unit conversion.
+     */
     private static final long KBYTE = 1024L;
+
+    /**
+     * Character representing the unit for kilobytes.
+     */
     private static final char K_UNIT = 'K';
+
+    /**
+     * Character representing the unit for bytes.
+     */
     private static final char B_UNIT = 'b';
 
+    /**
+     * Logger.
+     */
     private final Log log;
 
+    /**
+     * Character representing the unit of measurement used to display download progress.
+     */
     private char unit;
+
+    /**
+     * Represents the total length of the resource content being downloaded (in bytes).
+     */
     private long total;
+
+    /**
+     * Represents the number of bytes that have been successfully downloaded.
+     */
     private long completed;
 
-    public LoggingProgressReport(final Log log) {
-        this.log = log;
+    /**
+     * Constructor.
+     * @param logger Logger.
+     */
+    public LoggingProgressReport(final Log logger) {
+        this.log = logger;
     }
 
     @Override
-    public void initiate(URI uri, long total) {
-        this.total = total;
+    public void initiate(final URI uri, final long totalBytes) {
+        this.total = totalBytes;
         this.completed = 0L;
-        this.unit = total >= KBYTE ? K_UNIT : B_UNIT;
-        this.log.info(String.format( "%s: %s", "Downloading", uri));
+        this.unit = totalBytes >= KBYTE ? K_UNIT : B_UNIT;
+        this.log.info(String.format("%s: %s", "Downloading", uri));
     }
 
     @Override
-    public void update(long bytesRead) {
+    public void update(final long bytesRead) {
         this.completed += bytesRead;
         final String totalInUnits;
         final long completedInUnits;
-        if (unit == K_UNIT) {
-            totalInUnits = total == -1 ? "?" : Long.toString(total / KBYTE) + unit;
-            completedInUnits = completed / KBYTE;
+        if (this.unit == K_UNIT) {
+            totalInUnits = this.total == -1L
+                ? "?" : Long.toString(this.total / KBYTE) + this.unit;
+            completedInUnits = this.completed / KBYTE;
         } else {
-            totalInUnits = total == -1 ? "?" : Long.toString(total);
-            completedInUnits = completed;
+            totalInUnits = this.total == -1L ? "?" : Long.toString(this.total);
+            completedInUnits = this.completed;
         }
         this.log.info(String.format("%d/%s", completedInUnits, totalInUnits));
     }
 
     @Override
     public void completed() {
-        this.log.info(String.format("%s %s",
-                "downloaded",
-                unit == K_UNIT ? Long.toString(completed / KBYTE) + unit : Long.toString(completed)));
+        this.log.info(
+            String.format(
+                "%s downloaded",
+                LoggingProgressReport.render(this.completed, this.unit)
+            )
+        );
     }
 
     @Override
-    public void error(Exception exc) {
+    public void error(final Exception exc) {
         this.log.error(exc);
+    }
+
+    /**
+     * Converts a byte count into a string representation, optionally converting to kilobytes
+     * based on the specified unit.
+     *
+     * @param bytes The number of bytes.
+     * @param unit The unit of measurement ('K' for kilobytes, 'b' for bytes).
+     * @return The string representation of the byte count, optionally converted to kilobytes.
+     */
+    private static String render(final long bytes, final char unit) {
+        return unit == K_UNIT ? Long.toString(bytes / KBYTE) + unit : Long.toString(bytes);
     }
 }
--- src/main/java/com/googlecode/download/maven/plugin/internal/cache/DownloadCache.java
+++ src/main/java/com/googlecode/download/maven/plugin/internal/cache/DownloadCache.java
@@ -1,105 +1,134 @@
 /*
- * Copyright 2012, Red Hat Inc.
+ * Copyright 2009-2018 The Apache Software Foundation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.googlecode.download.maven.plugin.internal.cache;
 
 import com.googlecode.download.maven.plugin.internal.checksum.Checksums;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 
 /**
- * A class representing a download cache
+ * A class representing a download cache.
  * @author Mickael Istria (Red Hat Inc)
- *
  */
 public final class DownloadCache {
 
-	private final File basedir;
+    /**
+     * Directory where the download cache is stored.
+     */
+    private final File basedir;
+
+    /**
+     * Index of cached files in the download cache, mapping URIs to local file paths.
+     * This index enables fast retrieval of cached files and ensures thread-safe
+     * access through locking mechanisms.
+     */
     private final FileIndex index;
 
-	public DownloadCache(File cacheDirectory, Log log) {
+    /**
+     * Constructor.
+     * @param cacheDirectory Directory where the download cache is stored.
+     * @param log Logger.
+     */
+    public DownloadCache(final File cacheDirectory, final Log log) {
         this.index = new FileBackedIndex(cacheDirectory, log);
         this.basedir = cacheDirectory;
     }
 
-	private String getEntry(URI uri, final Checksums checksums) {
-		final String res = this.index.get(uri);
-		if (res == null) {
-			return null;
-		}
-		final File resFile = new File(this.basedir, res);
-		if (resFile.isFile() && checksums.isValid(resFile)) {
-			return res;
-		} else {
-			return null;
-		}
-	}
+    /**
+     * Get a File in the download cache. If no cache for this URL, or
+     * if expected checksums don't match cached ones, returns null.
+     * available in cache,
+     * @param uri URL of the file
+     * @param checksums Supplied checksums.
+     * @return A File when cache is found, null if no available cache
+     */
+    public File getArtifact(final URI uri, final Checksums checksums) {
+        final Optional<String> resource;
+        this.index.getLock().lock();
+        try {
+            resource = this.getEntry(uri, checksums);
+        } finally {
+            this.index.getLock().unlock();
+        }
+        return resource.map(res -> new File(this.basedir, res)).orElse(null);
+    }
 
-	/**
-	 * Get a File in the download cache. If no cache for this URL, or
-	 * if expected checksums don't match cached ones, returns null.
-	 * available in cache,
-	 * @param uri URL of the file
-	 * @param checksums Supplied checksums.
-	 * @return A File when cache is found, null if no available cache
-	 */
-    public File getArtifact(URI uri, final Checksums checksums) {
-		final String res;
-		this.index.getLock().lock();
-		try {
-			res = this.getEntry(uri, checksums);
-		} finally {
-			this.index.getLock().unlock();
-		}
-		if (res != null) {
-			return new File(this.basedir, res);
-		}
-		return null;
-	}
+    /**
+     * Installs a file into the download cache.
+     * If the cache directory does not exist, it is created.
+     * The method checks if the file corresponding to the URI and checksums already exists in the
+     * cache. If not, the file is copied to the cache directory and the index is updated.
+     *
+     * @param uri The URI of the file to be installed.
+     * @param outputFile The file to be installed into the cache.
+     * @param checksums The checksums used to verify the integrity of the file.
+     * @throws MojoFailureException If the cache directory cannot be created.
+     * @throws IOException If an I/O error occurs while copying the file.
+     */
+    public void install(final URI uri, final File outputFile, final Checksums checksums)
+        throws MojoFailureException, IOException {
+        if (!this.basedir.exists() && !this.basedir.mkdirs()) {
+            throw new MojoFailureException(
+                String.format(
+                    "Could not create cache directory: %s", this.basedir.getAbsolutePath()
+                )
+            );
+        }
+        this.index.getLock().lock();
+        try {
+            final Optional<String> entry = this.getEntry(uri, checksums);
+            if (!entry.isPresent()) {
+                final String fileName = String.format(
+                    "%s_%s", outputFile.getName(), DigestUtils.md5Hex(uri.toString())
+                );
+                Files.copy(
+                    outputFile.toPath(),
+                    new File(this.basedir, fileName).toPath(),
+                    StandardCopyOption.REPLACE_EXISTING
+                );
+                this.index.put(uri, fileName);
+            }
+        } finally {
+            this.index.getLock().unlock();
+        }
+    }
 
-    public void install(URI uri, File outputFile, final Checksums checksums) throws MojoFailureException, IOException {
-		if (!basedir.exists()) {
-			if (!basedir.mkdirs()) {
-				throw new MojoFailureException("Could not create cache directory: " + basedir.getAbsolutePath());
-			}
-		}
-		this.index.getLock().lock();
-		try {
-			final String entry = this.getEntry(uri, checksums);
-			if (entry != null) {
-				return; // entry already here
-			}
-			final String fileName = String.format(
-				"%s_%s", outputFile.getName(), DigestUtils.md5Hex(uri.toString())
-			);
-			Files.copy(
-				outputFile.toPath(),
-				new File(this.basedir, fileName).toPath(),
-				StandardCopyOption.REPLACE_EXISTING
-			);
-			// update index
-			this.index.put(uri, fileName);
-		} finally {
-			this.index.getLock().unlock();
-		}
-	}
+    /**
+     * Retrieves an entry from the cache index based on the provided URI and verifies it
+     * using the given checksums.
+     * @param uri The URI of the resource to retrieve.
+     * @param checksums The checksums used to validate the integrity of the resource.
+     * @return An Optional containing the resource path if the resource is valid and found,
+     *  otherwise an empty Optional.
+     */
+    private Optional<String> getEntry(final URI uri, final Checksums checksums) {
+        final String resource = this.index.get(uri);
+        return Optional.ofNullable(resource)
+            .filter(
+                res -> {
+                    final File resFile = new File(this.basedir, resource);
+                    return resFile.isFile() && checksums.isValid(resFile);
+                }
+            );
+    }
 }
